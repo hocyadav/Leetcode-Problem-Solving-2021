43a7475bc758ef88f3fed3f38725af5c632394e9
Hariom Yadav <hoc.yadav@gmail.com>
2021-02-23 12:15:37 +0530

    updated

---
+++ src/test/java/io/hari/problemsolving2021/ProblemSolving2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hari.problemsolving2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemSolving2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
