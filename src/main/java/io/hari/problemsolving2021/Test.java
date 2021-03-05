fd5b1c2125d716ae122ed0e3b25fcc723c8808bc
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-05 17:10:09 +0530

    generic link list impl

---
+++ src/main/java/io/hari/problemsolving2021/Test.java
@@ -0,0 +1,13 @@
+package io.hari.problemsolving2021;
+
+import lombok.val;
+
+public class Test {
+
+    public static void main(String[] args) {
+        final String hariom = "hariom yafav";
+        final String[] split = hariom.split("\\s");
+        System.out.println("split.length = " + split.length);
+    }
+
+}
