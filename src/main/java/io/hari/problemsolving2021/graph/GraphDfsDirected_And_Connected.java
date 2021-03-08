dd49ec779254d866e8f98b37784c091e828fde36
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-08 12:51:54 +0530

    graph level order / bfs traversal

--- src/main/java/io/hari/problemsolving2021/graph/GraphDfsDirected_And_Connected.java
+++ src/main/java/io/hari/problemsolving2021/graph/GraphDfsDirected_And_Connected.java
@@ -3,6 +3,7 @@ package io.hari.problemsolving2021.graph;
 import lombok.Getter;
 
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.stream.Stream;
 
 /**
@@ -12,22 +13,23 @@ import java.util.stream.Stream;
 @Getter
 class Graph {
     int nodeSize;
-    LinkedList<Integer>[] linkedLists = new LinkedList[4];
+    LinkedList<Integer>[] listAdj = new LinkedList[4];
 
     public Graph(int nodeSize) {
         this.nodeSize = nodeSize;
-        this.linkedLists = new LinkedList[nodeSize];
+        this.listAdj = new LinkedList[nodeSize];
         for (int i = 0; i < nodeSize; i++) { // allocate space for each node
-            linkedLists[i] = new LinkedList<>();
+            listAdj[i] = new LinkedList<>();
         }
     }
 
     public void addEdge(final int source, final int destination) {
-        linkedLists[source].add(destination);
+        listAdj[source].add(destination);
     }
 
 
     public void dfsConnected(final int startNode) { // goal traverse all node with start index given
+        System.out.println("Graph.dfsConnected");
         boolean[] visited = new boolean[nodeSize];//if we take Boolean then all values will be null
         for (boolean b : visited) {
             System.out.println("b = " + b);//we will get false, since this is primitive type
@@ -40,7 +42,29 @@ class Graph {
         System.out.println();
     }
 
+    public void bfsLevelOrderConnected(final int startNode) {
+        System.out.println("Graph.bfsLevelOrderConnected");
+        boolean[] visited = new boolean[nodeSize];
+
+        Queue<Integer> qq = new LinkedList<>();
+        qq.add(startNode);
+        visited[startNode] = true;
+
+        while (!qq.isEmpty()) {
+            final Integer curr = qq.poll();
+            System.out.print(curr + " ");
+            for (Integer childNode : this.listAdj[curr]) {
+                if (visited[childNode] == false) {//if not visited then mark visited and add to queue
+                    visited[childNode] = true;
+                    qq.add(childNode);
+                }
+            }
+        }
+        System.out.println();
+    }
+
     public void dfsNotConnected() { // no start index, it has more than one graph component, goal : traverse all node
+        System.out.println("Graph.dfsNotConnected");
         boolean[] visited = new boolean[nodeSize];
         for (int i = 0; i < nodeSize; i++) {
             if (visited[i] == false) dfsUtil(i, visited);
@@ -55,7 +79,7 @@ class Graph {
         System.out.print(currNode + " ");
 
         //get all connected nodes with current start node
-        final LinkedList<Integer> linkedList = linkedLists[currNode];
+        final LinkedList<Integer> linkedList = listAdj[currNode];
         for (Integer node : linkedList) {
             if (visited[node] == false) {
                 dfsUtil(node, visited);
@@ -77,7 +101,7 @@ public class GraphDfsDirected_And_Connected {//connected means only on graph
         graph.addEdge(3, 1);
         graph.addEdge(3, 2);
 
-        final LinkedList<Integer>[] linkedLists = graph.getLinkedLists();
+        final LinkedList<Integer>[] linkedLists = graph.getListAdj();
         Stream.of(linkedLists).forEach(i -> {
             final LinkedList<Integer> i1 = i;
             System.out.println("i1 = " + i1);
@@ -85,10 +109,11 @@ public class GraphDfsDirected_And_Connected {//connected means only on graph
 
         graph.dfsConnected(0);
         graph.dfsNotConnected();
+        graph.bfsLevelOrderConnected(0);
 
         // todo disconnected nodes
         Graph graph2 = new Graph(6);
-        graph2.addEdge(0,1); //graph component 1
+        graph2.addEdge(0, 1); //graph component 1
         graph2.addEdge(1, 2); // graph component 2
         graph2.addEdge(4, 5); // graph component 3
         graph2.dfsNotConnected();
