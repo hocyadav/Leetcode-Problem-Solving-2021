d91ed3df5ddde8cda9eaa64ea1916f172263fa8f
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-11 12:35:37 +0530

    scanner class in java - read from console

---
+++ src/main/java/io/hari/problemsolving2021/ScannerReadFromConsole.java
@@ -0,0 +1,25 @@
+package io.hari.problemsolving2021;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @Author Hariom Yadav
+ * @create 11-03-2021
+ */
+public class ScannerReadFromConsole {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final String nextLine = scanner.nextLine();//send str input like : hariom yadav 9887700499
+        System.out.println("nextLine = " + nextLine);
+        //split by spaces and read each str
+        final String[] split = nextLine.split(" ");
+        Arrays.stream(split).forEach(i -> System.out.println("i = " + i));
+
+        final String next = scanner.next();//read only 1st string , if input is hariomm yadav then it will read only hariom
+        System.out.println("next = " + next);
+
+        final int nextInt = scanner.nextInt();//this is continuous of above scanner.next, if we put "hari om" then error "hari 123" is correct
+        System.out.println("nextInt = " + nextInt);
+    }
+}
