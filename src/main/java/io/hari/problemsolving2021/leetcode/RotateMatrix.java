9ccdeccdb243bbc95d7b16e329eb132aa109411b
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-17 18:43:51 +0530

    RotateMatrix

---
+++ src/main/java/io/hari/problemsolving2021/leetcode/RotateMatrix.java
@@ -0,0 +1,58 @@
+package io.hari.problemsolving2021.leetcode;
+
+/**
+ * @Author Hariom Yadav
+ * @create 17-03-2021
+ */
+public class RotateMatrix {
+    public static void main(String[] args) {
+        int[][] mat = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        printMatrix(mat);
+        printMatrix(mat);
+        swapUpsideDown(mat);
+        printMatrix(mat);
+        swapSymmetricWiseDiagonalWise(mat);
+        printMatrix(mat);
+    }
+
+    /**
+     * Approach : Use simple swap logic, take single row as single value
+     */
+    private static void swapUpsideDown(int[][] mat) {
+        int p1 = 0, p2 = mat.length - 1;
+        while (p1 < p2) {
+            int[] t = mat[p1];//single row will become one value
+            mat[p1] = mat[p2];
+            mat[p2] = t;
+            p1++; p2--;
+        }
+    }
+
+    private static void swapSymmetricWiseDiagonalWise(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            System.out.println("i = " + i);
+            for (int j = i + 1; j < mat[i].length; j++) {
+//                System.out.println("j = " + j);
+                System.out.println("swapping "+i +" "+j);
+                int t = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = t;
+            }
+        }
+    }
+
+    private static void printMatrix(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------");
+    }
+
+}
