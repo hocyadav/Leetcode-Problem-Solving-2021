51dbfd2beec1aace7b4d35c2f45480f9ac90872f
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-18 07:58:24 +0530

    comment

--- src/main/java/io/hari/problemsolving2021/leetcode/KadensAlgo_MaxSumOfAnyWindowSize.java
+++ src/main/java/io/hari/problemsolving2021/leetcode/KadensAlgo_MaxSumOfAnyWindowSize.java
@@ -13,6 +13,11 @@ public class KadensAlgo_MaxSumOfAnyWindowSize {
         final int sum3 = maxSumOfAnyWindowSize(new int[]{1, 2, 3, -4, 5, -10, -1, 100});//100
         System.out.println("sum3 = " + sum3);
 
+        final int i = maxSumOfAnyWindow_UsingSlidingWindow(new int[]{1, 2, 3, -4});
+        System.out.println("sum4 ~ sum= " + i);
+        System.out.println("sum5 ~ sum2= " + maxSumOfAnyWindow_UsingSlidingWindow(new int[]{1, 2, 3, -4, 5, -10, -1}));
+        System.out.println("sum5 ~ sum3 " + maxSumOfAnyWindow_UsingSlidingWindow(new int[]{1, 2, 3, -4, 5, -10, -1, 100}));
+
     }
 
     /** Algorithm
@@ -37,9 +42,30 @@ public class KadensAlgo_MaxSumOfAnyWindowSize {
         }
         return globalSum;
     }
+
+    /**
+     * 1. k = 1 , take 1st value as max windows sum
+     * 2. traverse and calculate localsum
+     * 3. reset localsum if < 0
+     * 4. update result
+     */
+    public static int maxSumOfAnyWindow_UsingSlidingWindow(int[] arr) {
+        int result = arr[0];//1st window sum, this is k = 1 for us
+
+        int localSum = result;
+        for (int i = 1; i < arr.length; i++) {//2. start from 2nd value
+            localSum += arr[i];//1. update localsum
+            if (localSum < 0) localSum = 0;//2 reset localSum id < 0
+            result = Math.max(result, localSum);// 3. update result
+        }
+        return result;
+    }
 }
 /**
  sum = 6
  sum2 = 7
  sum3 = 100
+ sum4 ~ sum= 6
+ sum5 ~ sum2= 7
+ sum5 ~ sum3 100
  */
