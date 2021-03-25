ac33afad44b03857beb765c07fdfdfe9c127f004
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-25 07:44:28 +0530

    GenerateParentheses

---
+++ src/main/java/io/hari/problemsolving2021/backtracking/GenerateParentheses.java
@@ -0,0 +1,42 @@
+package io.hari.problemsolving2021.backtracking;
+
+import lombok.val;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author Hariom Yadav
+ * @create 25-03-2021
+ */
+public class GenerateParentheses {
+    static List<String> result = new LinkedList<>();
+    public static void main(String[] args) {
+        final int length = 3;
+        generateParenthesesOfLen(length);
+        System.out.println("result = " + result);
+        generateParenthesesOfLen(1);
+        System.out.println("result = " + result);
+    }
+
+    private static void generateParenthesesOfLen(int len) {
+        result.clear();
+        int leftLen = len;
+        int rightLen = len;
+        rec("", leftLen, rightLen);
+    }
+
+    private static void rec(String current, int leftLen, int rightLen) {
+        if (leftLen > rightLen) return;//null check, valid invalid check
+        if (leftLen == 0 && rightLen == 0) { //small problem solution
+            result.add(current);
+            return;
+        }
+        if (leftLen > 0) rec(current + "(", leftLen - 1, rightLen);
+        if (rightLen > 0) rec(current + ")", leftLen, rightLen - 1);
+    }
+}
+/**
+ result = [((())), (()()), (())(), ()(()), ()()()]
+ result = [()]
+ */
