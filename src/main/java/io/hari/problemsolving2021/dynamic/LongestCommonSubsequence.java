8b84094ae6c4015938f854f223d1892fb53bbf81
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-30 07:38:43 +0530

    LongestCommonSubsequence

--- src/main/java/io/hari/problemsolving2021/dynamic/LongestCommonSubsequence.java
+++ src/main/java/io/hari/problemsolving2021/dynamic/LongestCommonSubsequence.java
@@ -5,39 +5,46 @@ import static io.hari.problemsolving2021.leetcode.WallsAndGate_GraphProblem_n_01
 /**
  * @Author Hariom Yadav
  * @create 30-03-2021
+ * https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
  */
 public class LongestCommonSubsequence {
     public static void main(String[] args) {
         String str1 = "abcde";
         String str2 = "bcde";
-        final int lcsLength = recLCSAlgo(str1, str2, str1.length() - 1, str2.length() - 1);//start rec from last
+        //start rec from last
+        final int lcsLength = recLCSAlgo(str1, str2, str1.length(), str2.length());
         System.out.println("lcsLength = " + lcsLength);
 
-        recLCSAlgo_withMemoization(str1, str2, str1.length() - 1, str2.length() - 1);//same as above just added dp array
+        recLCSAlgo_withMemoization(str1, str2, str1.length(), str2.length());//same as above just added dp array
+
+        System.out.println("lcsLength2 = " + recLCSAlgo("hariom", "", "hariom".length(), "".length()));
+        System.out.println("lcsLength3 = " + recLCSAlgo("hariom", "ha", "hariom".length(), "ha".length()));
+        System.out.println("lcsLength3 = " + recLCSAlgo("hariom", "xyzha123", "hariom".length(), "xyzha123".length()));
+
     }
 
-    private static void recLCSAlgo_withMemoization(String str1, String str2, int lastIndexS1, int lastIndexS2) {
+    private static void recLCSAlgo_withMemoization(String str1, String str2, int s1len, int s2len) {
         int[][] dp = new int[str1.length() + 1][str2.length() + 1];
-        recUtil(str1, str2, lastIndexS1, lastIndexS2, dp);
+        recUtil(str1, str2, s1len, s2len, dp);
         print2DArray(dp);
-        System.out.println("dp[lastIndexS1][lastIndexS2] = " + dp[lastIndexS1][lastIndexS2]);
+        System.out.println("dp[s1len][s2len] = " + dp[s1len][s2len]);
     }
 
-    private static int recUtil(String str1, String str2, int lastIndexS1, int lastIndexS2, int[][] dp) {
-        if (lastIndexS1 == 0 || lastIndexS2 == 0) {
-            dp[lastIndexS1][lastIndexS2] = 0;
-            return dp[lastIndexS1][lastIndexS2];
+    private static int recUtil(String str1, String str2, int s1len, int s2len, int[][] dp) {
+        if (s1len == 0 || s2len == 0) {
+            dp[s1len][s2len] = 0;
+            return dp[s1len][s2len];
         }
         ;
-        if (str1.charAt(lastIndexS1) == str2.charAt(lastIndexS2)) {
-            final int oneStepBack = 1 + recUtil(str1, str2, lastIndexS1 - 1, lastIndexS2 - 1, dp);
-            dp[lastIndexS1][lastIndexS2] = oneStepBack;
-            return dp[lastIndexS1][lastIndexS2];
+        if (str1.charAt(s1len - 1) == str2.charAt(s2len - 1)) {
+            final int oneStepBack = 1 + recUtil(str1, str2, s1len - 1, s2len - 1, dp);
+            dp[s1len][s2len] = oneStepBack;
+            return dp[s1len][s2len];
         } else {
-            final int case1 = recUtil(str1, str2, lastIndexS1 - 1, lastIndexS2, dp);
-            final int case2 = recUtil(str1, str2, lastIndexS1, lastIndexS2 - 1, dp);
-            dp[lastIndexS1][lastIndexS2] = Math.max(case1, case2);
-            return dp[lastIndexS1][lastIndexS2];
+            final int case1 = recUtil(str1, str2, s1len - 1, s2len, dp);
+            final int case2 = recUtil(str1, str2, s1len, s2len - 1, dp);
+            dp[s1len][s2len] = Math.max(case1, case2);
+            return dp[s1len][s2len];
         }
     }
 
@@ -47,28 +54,31 @@ public class LongestCommonSubsequence {
      * if same : 1 + call recursion and subtract that common char
      * if not same : 2 cases : take last char s1 and dont take last char s2,case 2: negation of case1
      */
-    private static int recLCSAlgo(String str1, String str2, int lastIndexS1, int lastIndexS2) {
-        if (lastIndexS1 == 0 || lastIndexS2 == 0) return 0;
+    private static int recLCSAlgo(String str1, String str2, int s1len, int s2len) {
+        if (s1len == 0 || s2len == 0) return 0;
 
-        if (str1.charAt(lastIndexS1) == str2.charAt(lastIndexS2)) {//last char matched
-            final int oneStepBack = 1 + recLCSAlgo(str1, str2, lastIndexS1 - 1, lastIndexS2 - 1);
+        if (str1.charAt(s1len - 1) == str2.charAt(s2len - 1)) {//last char matched
+            final int oneStepBack = 1 + recLCSAlgo(str1, str2, s1len - 1, s2len - 1);
             return oneStepBack;
         } else {
-            final int case1 = recLCSAlgo(str1, str2, lastIndexS1 - 1, lastIndexS2);
-            final int case2 = recLCSAlgo(str1, str2, lastIndexS1, lastIndexS2 - 1);
+            final int case1 = recLCSAlgo(str1, str2, s1len - 1, s2len);
+            final int case2 = recLCSAlgo(str1, str2, s1len, s2len - 1);
             return Math.max(case1, case2);
         }
     }
 }
 /**
- lcsLength = 3
+ lcsLength = 4
  0 0 0 0 0
  0 0 0 0 0
  0 1 0 0 0
  0 0 2 0 0
  0 0 0 3 0
- 0 0 0 0 0
+ 0 0 0 0 4
  --------------
- dp[lastIndexS1][lastIndexS2] = 3
+ dp[s1len][s2len] = 4
+ lcsLength2 = 0
+ lcsLength3 = 2
+ lcsLength3 = 2
 
  */
